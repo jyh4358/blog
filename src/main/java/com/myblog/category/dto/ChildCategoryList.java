16050ebc49ff009c3214309305a2a0a9e20a68c8
jyh4358 <jyh4358@gmail.com>
2022-06-20 20:04:14 +0900

    feat: 카테고리 수정, 추가 구현

---
+++ src/main/java/com/myblog/category/dto/ChildCategoryList.java
@@ -0,0 +1,19 @@
+package com.myblog.category.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ChildCategoryList {
+    private Long id;
+    private String childCategory;
+    private Boolean deleteCheck;
+
+    public boolean isNewCategory(){
+        return id == null;
+    }
+}
