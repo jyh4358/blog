6f207b958b4e8ef1b3a76145161eb58ccd7fe203
jyh4358 <jyh4358@gmail.com>
2022-07-02 12:46:47 +0900

    feat: thumbnail api 요청 리팩토링

--- src/main/resources/static/js/thumbnail.js
+++ src/main/resources/static/js/thumbnail.js
@@ -1,54 +1,49 @@
+let thumbnailIndex = {
+    init: function () {
+        let _this = this;
+        $("#thumbnail").on("change", function () {
+            _this.uploadThumbnail();
+        });
+        $("#thumbnail-url-upload-btn").on("click", function () {
+            _this.uploadThumbnailUrl();
+        });
+        $("#thumbDelBtn").on("click", function () {
+            _this.deleteThumbnail();
+        });
 
-const thumbBox = document.getElementById("thumbBox");
-const uploadThumbBtn = document.getElementById("thumbnail");
-const thumbDel = document.getElementById("thumbDelBtn");
-const previewThumb = document.getElementById("thumbnailPreView");
-const thumbUrl = document.getElementById("thumbnailUrl");
-const thumbUrlUploadBtn = document.getElementById("thumbnail-url-upload-btn");
-
-function uploadImg(input) {
-
-    if(input.files && input.files[0]) {
-
+    },
+    uploadThumbnail: function () {
         let token = getCsrfToken();
         let formData = new FormData();
-        formData.append('file', input.files[0]);
+        formData.append('file', $("#thumbnail")[0].files[0]);
 
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "/file/upload", false);
-        xhr.setRequestHeader("contentType", "multipart/form-data");
-        xhr.setRequestHeader("X-XSRF-TOKEN", token);
-        xhr.send(formData);
-
-        if (xhr.readyState === 4 && xhr.status === 200) {
-
-            thumbUrl.value = xhr.response;
-            previewThumb.src = thumbUrl.value;
-
-            thumbBox.style.display = ''
-
-        } else {
-            alert("이미지가 정상적으로 업로드되지 못했습니다.")
-        }
-
-    }
+        $.ajax({
+            type: 'POST',
+            url: '/file/upload',
+            headers: {'X-XSRF-TOKEN': token},
+            processData: false,
+            contentType: false,
+            async: false,
+            data: formData
+        }).done(function (result) {
+            $("#thumbnailUrl").val(result);
+            $("#thumbnailPreView").attr("src", result);
+            $("#thumbBox").css("display", "");
+            $('#thumbnailModal').modal('hide');
+        }).fail(function (error) {
+            alert(error.responseJSON.errorMessage);
+        });
+    },
+    uploadThumbnailUrl: function () {
+        let thumbnailUrl = $("#thumbnail-url-upload-input").val();
+        $("#thumbnailUrl").val(thumbnailUrl)
+        $("#thumbnailPreView").attr("src", thumbnailUrl);
+        $("#thumbBox").css("display", "");
+        $('#thumbnailModal').modal('hide');
+    },
+    deleteThumbnail: function () {
+        $("#thumbBox").css("display", "none");
+        $("#thumbnailUrl").val("");
+    },
 }
-
-
-
-thumbUrlUploadBtn.addEventListener("click", () =>{
-    const thumbUrlUploadInput = document.getElementById("thumbnail-url-upload-input");
-    const url = thumbUrlUploadInput.value;
-    previewThumb.src = url;
-    thumbUrl.value = url;
-    thumbBox.style.display = '';
-})
-
-uploadThumbBtn.addEventListener("change", e => {
-    uploadImg(e.target);
-})
-
-thumbDel.addEventListener("click", () =>{
-    thumbBox.style.display = 'none';
-    thumbUrl.value = "";
-})
+thumbnailIndex.init();
