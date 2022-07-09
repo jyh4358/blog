1613b1959a45c7acf6e0d48b83d74c66840ea4f1
jyh4358 <jyh4358@gmail.com>
2022-07-09 15:42:38 +0900

    feat: profile 컨트롤러 추가

---
+++ src/main/java/com/myblog/common/ProfileController.java
@@ -0,0 +1,26 @@
+package com.myblog.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        return Arrays.stream(env.getActiveProfiles())
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse("");
+    }
+}
