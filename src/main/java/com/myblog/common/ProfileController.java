d27ad721f02e02f796b7fd8ffdfac67b518f9686
jyh4358 <jyh4358@gmail.com>
2022-07-09 15:45:23 +0900

    feat: profile 컨트롤러 추가

--- src/main/java/com/myblog/common/ProfileController.java
+++ src/main/java/com/myblog/common/ProfileController.java
@@ -18,6 +18,7 @@ public class ProfileController {
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
+
         return Arrays.stream(env.getActiveProfiles())
                 .filter(realProfiles::contains)
                 .findAny()
