53905c48b821b76d40441191b9295bf88aa0eb77
jyh4358 <jyh4358@gmail.com>
2022-07-09 15:50:40 +0900

    feat: webhook 테스트

--- src/main/java/com/myblog/common/ProfileController.java
+++ src/main/java/com/myblog/common/ProfileController.java
@@ -18,7 +18,6 @@ public class ProfileController {
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real1", "real2");
-
         return Arrays.stream(env.getActiveProfiles())
                 .filter(realProfiles::contains)
                 .findAny()
